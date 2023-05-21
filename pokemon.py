33bea4af7723a445043e148332f5dd9603d183e3
Soni <axel.atelier@gmail.com>
2023-05-21 09:09:05 -0600

    Se agrego el tipo de pokemon que puede ser

--- pokemon.py
+++ pokemon.py
@@ -1,2 +1,12 @@
 pokemon = input("Dame el nombre de un pokemon ") 
-print(f"Tu pokemon se llama {pokemon}")
+election = int(input("Selecciona un numero del 1 al 3 "))
+style = None
+
+
+if election == 1:
+    style = str("fuego")
+else:
+    style = str("agua")
+
+
+print(f"Tu pokemon se llama {pokemon} y su tipo es {style}")
